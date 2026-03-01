e021a3f648bc5a0b10530989a33876b0993ed26e
Michal Forman <miska.forman@gmail.com>
2026-03-01 14:06:29 +0100

    add logs to main

--- main.py
+++ main.py
@@ -13,10 +13,12 @@ from datetime import datetime
 
 
 #----- Start counting time for a run -----
+log.info("Starting Main scrpit")
 run_start = datetime.utcnow()
 
 #----- Initialization -----
 
+log.info("Initializing all classes")
 t212 = Trading212(api_id_key=settings.t212_id_key, api_private_key=settings.t212_private_key, demo=False)
 coinmate = Coinmate(settings.coinmate_client_id, settings.coinmate_public_key, settings.coinmate_private_key)
 instruments = Instruments(t212=t212, portfolio_settings=settings.portfolio)
@@ -25,11 +27,14 @@ executor = Executor(t212, coinmate, settings.portfolio)
 #----- Main program logic -----
 
 # Update values in db based on current state - Do as often as possible = on every script run
+log.info("Updating old Orders and Runs")
 Order.update_orders(t212, coinmate)
 Run.update_runs()
+log.info("Updated old Orders and Runs successfully")
 
 # Create new orders if they should be made today AND they have not yet been
 if is_now_cron_time(settings.portfolio.invest_interval) and not Run.run_exists_today():
+    log.info("Starting investment process")
 
     # Init new run
     run = Run.create_run(run_start)
@@ -40,13 +45,17 @@ if is_now_cron_time(settings.portfolio.invest_interval) and not Run.run_exists_t
     cash_distribution = calculated_investment["cash_distribution"]
     multipliers = calculated_investment["multipliers"]
     orders: List[Order] = executor.place_orders(cash_distribution, multipliers, run_id=run.id)
+    log.info("Investment process finished successfully")
 
     # Update the run data with info about the orders
     run_data_for_update: RunUpdate = run.process_new_run_data(orders)
     try:
         run.update_in_db(run_data_for_update)
+        log.info("Updated Run data based on Orders made successfully")
     except Exception as e:
         log.error(f"Failed to update the db, error: {e}")
+else:
+    log.info("No were supposed to be made in this run")
 
 
 
